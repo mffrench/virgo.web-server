b70b67dc23910c84185e8b7dbdc823e13bdef4fe
Steve Powell <spowell@vmware.com>
2010-06-10 17:26:39 +0100

    Update releaselor for virgo releases

--- scripts/releaselor/options.rb
+++ scripts/releaselor/options.rb
@@ -1,37 +1,38 @@
 require "rubygems"
 require "choice"
+require "etc"
 
 Choice.options do
 
-  header ''
-  header 'Required arguments:'
+  header('')
+  header('Required arguments:')
 
   option :version, :required => true do
-    short '-v'
-    long '--version=VERSION'
-    desc 'The version number for the release'
+    short('-v')
+    long('--version=VERSION')
+    desc('The version number for the release')
   end
 
   option :build_stamp, :required => true do
-    short '-s'
-    long '--build-stamp=BUILD-STAMP'
-    desc 'The build stamp for the release'
+    short('-s')
+    long('--build-stamp=BUILD-STAMP')
+    desc('The build stamp for the release')
   end
 
   option :release_type, :required => true do
-    short '-t'
-    long '--release-type=RELEASE-TYPE'
-    desc 'The release type for the release'
+    short('-t')
+    long('--release-type=RELEASE-TYPE')
+    desc('The release type for the release')
   end
 
   option :new_version, :required => true do
-    short '-n'
-    long '--new-version=NEW-VERSION'
-    desc 'The new version number for the release'
+    short('-n')
+    long('--new-version=NEW-VERSION')
+    desc('The new version number for the release')
   end
 
-  separator ''
-  separator 'Optional arguments:'
+  separator('')
+  separator('Optional arguments:')
 
   option :repository_map, :required => false do
     short('-m')
@@ -41,20 +42,33 @@ Choice.options do
     desc('(defaults to ~/repository.map)')
   end
 
+  option :publish_keys, :required => false do
+    short('-p')
+    long('--publish-keys=PUBLISH-KEYS')
+    default('~/publish.properties')
+    desc('The property file containing the ssh information used for publishing')
+    desc('(defaults to ~/publish.properties)')
+  end
+  
   option :s3_keys, :required => false do
-    short '-s'
-    long '--s3-keys=S3-KEYS'
-    default '~/s3.properties'
-    desc 'The property file containing the S3 keys used for publishing'
-    desc '(defaults to ~/s3.properties)'
+    short('-s')
+    long('--s3-keys=S3-KEYS')
+    default('~/s3.properties')
+    desc('The property file containing the S3 keys used for publishing')
+    desc('(defaults to ~/s3.properties)')
   end
 
-  option :publish_keys, :required => false do
-    short '-p'
-    long '--publish-keys=PUBLISH-KEYS'
-    default '~/publish.properties'
-    desc 'The property file containing the ssh information used for publishing'
-    desc '(defaults to ~/publish.properties)'
+  option :remote_user, :required => false do
+   short('-u')
+   long('--remote-user=REMOTE-USER')
+   default(Etc.getlogin)
+   desc('User id to use for remote repository access')
+   desc('(defaults to local login id)')
+  end
+   
+  option :dryrun?, :required => false do
+   long('--dry-run')
+   desc('Show what would happen but do not actually do anything')
   end
 
 end
--- scripts/releaselor/releaselor.rb
+++ scripts/releaselor/releaselor.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby -wKU
 $LOAD_PATH << File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH << File.expand_path(File.dirname(__FILE__) + '/../lib')
 
 require 'repository'
 require 'options'
 
 args = Choice.choices
 bundle_version = args[:version] + '.' + args[:build_stamp]
+
+DRY_RUN = args[:dryrun?].nil? ? false : true
+puts "This is a dry run..." if DRY_RUN
 
 if File.exist?(File.expand_path(args[:repository_map]))
   paths = Hash.new
@@ -14,28 +18,39 @@ if File.exist?(File.expand_path(args[:repository_map]))
   end
 else
   paths = {
+    'osgi-test-stubs' => 'osgi-test-stubs',
     'osgi-extensions' => 'osgi-extensions',
     'util' => 'util',
-    'artifact-repository' => 'artifact-repository',
     'test' => 'test',
+    'medic' => 'medic',
+    'artifact-repository' => 'artifact-repository',
     'kernel' => 'kernel',
-    'apps' => 'apps',
+    'kernel-tools' => 'kernel-tools',
     'web' => 'web',
+    'apps' => 'apps',
     'documentation' => 'documentation',
     'web-server' => 'web-server'
   }
 end
 
+#def initialize(repo_root, name, path, variable, bundle_version=nil, targets = 'clean clean-integration test publish', master_branch = 'master')
+
+local_repo_root = 'git@git.springsource.org:virgo/'
+eclipse_repo_root = 'ssh://' + args[:remote_user] + '@git.eclipse.org/gitroot/virgo/org.eclipse.virgo.'
+
 ALL_REPOS = [
-  Repository.new('osgi-extensions', paths['osgi-extensions'],'org.eclipse.virgo.osgi', bundle_version),
-  Repository.new('util', paths['util'], 'org.eclipse.virgo.util', bundle_version),
-  Repository.new('artifact-repository', paths['artifact-repository'], 'org.eclipse.virgo.repository', bundle_version),
-  Repository.new('test', paths['test'], 'org.eclipse.virgo.osgi.test', bundle_version),
-  Repository.new('kernel', paths['kernel'], 'org.eclipse.virgo.kernel', bundle_version, 'clean clean-integration test package publish'),
-  Repository.new('web', paths['web'], 'org.eclipse.virgo.web', bundle_version),
-  Repository.new('apps', paths['apps'], 'org.eclipse.virgo.apps', bundle_version),
-  Repository.new('documentation', paths['documentation'], 'org.eclipse.virgo.documentation', bundle_version, 'doc publish publish-static'),
-  Repository.new('web-server', paths['web-server'], nil, bundle_version, 'test package smoke-test publish')
+  Repository.new(eclipse_repo_root, 'osgi-test-stubs',     paths['osgi-test-stubs'],     'org.eclipse.virgo.teststubs',       bundle_version),
+  Repository.new(eclipse_repo_root, 'osgi-extensions',     paths['osgi-extensions'],     'org.eclipse.virgo.osgi',            bundle_version),
+  Repository.new(eclipse_repo_root, 'util',                paths['util'],                'org.eclipse.virgo.util',            bundle_version),
+  Repository.new(eclipse_repo_root, 'test',                paths['test'],                'org.eclipse.virgo.test',            bundle_version),
+  Repository.new(eclipse_repo_root, 'medic',               paths['medic'],               'org.eclipse.virgo.medic',           bundle_version),
+  Repository.new(eclipse_repo_root, 'artifact-repository', paths['artifact-repository'], 'org.eclipse.virgo.repository',      bundle_version),
+  Repository.new(eclipse_repo_root, 'kernel',              paths['kernel'],              'org.eclipse.virgo.kernel',          bundle_version,  'test package publish'),
+  Repository.new(eclipse_repo_root, 'kernel-tools',        paths['kernel-tools'],        'org.eclipse.virgo.kernel-tools',    bundle_version),
+  Repository.new(eclipse_repo_root, 'web',                 paths['web'],                 'org.eclipse.virgo.web',             bundle_version),
+  Repository.new(eclipse_repo_root, 'apps',                paths['apps'],                'org.eclipse.virgo.apps',            bundle_version),
+  Repository.new(eclipse_repo_root, 'documentation',       paths['documentation'],       'org.eclipse.virgo.documentation',   bundle_version,  'doc publish publish-static'),
+  Repository.new(eclipse_repo_root, 'web-server',          paths['web-server'],          nil,                                 bundle_version,  'test package smoke-test publish')
 ]
 
 log_file=File.expand_path('./release.log')
@@ -44,21 +59,33 @@ start_time = Time.new
 versions = Hash.new
 ALL_REPOS.each do |repo|
   puts 'Releasing ' + repo.name
+  puts '  checkout with "' + repo.clone_command + '"' if DRY_RUN
   repo.checkout
-  repo.create_release_branch(args[:version], args[:build_stamp], args[:release_type], versions)
-  repo.build(args[:s3_keys], args[:publish_keys], log_file)
-  repo.create_tag
-  repo.update_master_branch(args[:new_version], versions)
+  if DRY_RUN
+    puts "  Create Release branch " + args[:version] + ", " + args[:build_stamp] + ", " + args[:release_type] 
+    puts "    using versions: " + versions
+    puts "  Building " + repo.name + " (s3.keys, publish.keys)"
+    puts "  Create tag " + repo.bundle_version
+    puts "  Update Master branch " + args[:new_version]
+  else
+    repo.create_release_branch(args[:version], args[:build_stamp], args[:release_type], versions)
+    repo.build(args[:s3_keys], log_file, args[:publish_keys])
+    repo.create_tag
+    repo.update_master_branch(args[:new_version], versions)
+  end
   versions.merge!(repo.versions)
 end
 
-puts 'Execution Time: ' + Time.at(Time.new - start_time).utc.strftime('%R:%S')
-puts ''
+if !DRY_RUN
+  puts 'Execution Time: ' + Time.at(Time.new - start_time).utc.strftime('%R:%S')
+  puts ''
 
-print 'Do you want to push? '
-commit_ok = STDIN.gets.chomp
-if commit_ok =~ /y.*/
-  ALL_REPOS.each do |repo|
-    repo.push(args[:new_version])
+  print 'Do you want to push? (y/n) '
+  commit_ok = STDIN.gets.chomp
+  if commit_ok =~ /y.*/
+    ALL_REPOS.each do |repo|
+      repo.push(args[:new_version])
+    end
   end
 end
+
