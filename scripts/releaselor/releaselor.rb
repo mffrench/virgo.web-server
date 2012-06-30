ede46890724ec07ddf4cd2751216fb051aca1836
Borislav Kapukaranov <b.kapukaranov@gmail.com>
2012-06-30 03:35:41 +0300

    Fixes a wrong eclipse mirror repository definition

--- scripts/releaselor/releaselor.rb
+++ scripts/releaselor/releaselor.rb
@@ -54,7 +54,7 @@ default_targets = 'clean clean-integration test publish-ivy publish-maven'
 if args[:product_release] == 'full-product' 
 
   ALL_REPOS = [
-    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     nil, ripple_branch, 'clean'),
+    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     bundle_version, release_from_branch, 'clean'),
     Repository.new(virgo_eclipse_repo_root,  'osgi-test-stubs',      paths['osgi-test-stubs'],     'org.eclipse.virgo.teststubs',       bundle_version, release_from_branch,        default_targets),
     Repository.new(virgo_eclipse_repo_root,  'osgi-extensions',      paths['osgi-extensions'],     'org.eclipse.virgo.osgi',            bundle_version, release_from_branch,        default_targets),
     Repository.new(virgo_eclipse_repo_root,  'util',                 paths['util'],                'org.eclipse.virgo.util',            bundle_version, release_from_branch,        default_targets),
@@ -76,7 +76,7 @@ if args[:product_release] == 'full-product'
 elsif args[:product_release] == 'kernel' 
 
   ALL_REPOS = [
-    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     nil, ripple_branch, 'clean'),
+    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     bundle_version, release_from_branch, 'clean'),
     Repository.new(virgo_eclipse_repo_root, 'osgi-test-stubs',     paths['osgi-test-stubs'],     'org.eclipse.virgo.teststubs',       bundle_version, release_from_branch, default_targets),
     Repository.new(virgo_eclipse_repo_root, 'osgi-extensions',     paths['osgi-extensions'],     'org.eclipse.virgo.osgi',            bundle_version, release_from_branch, default_targets),
     Repository.new(virgo_eclipse_repo_root, 'util',                paths['util'],                'org.eclipse.virgo.util',            bundle_version, release_from_branch, default_targets),
@@ -91,7 +91,7 @@ elsif args[:product_release] == 'kernel'
 elsif args[:product_release] == 'web-server' 
   
   ALL_REPOS = [
-    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     nil, ripple_branch, 'clean'),
+    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     bundle_version, release_from_branch, 'clean'),
     Repository.new(virgo_eclipse_repo_root, 'web',                 paths['web'],                 'org.eclipse.virgo.web',             bundle_version, release_from_branch, default_targets),
     Repository.new(virgo_eclipse_repo_root, 'snaps',               paths['snaps'],               'org.eclipse.virgo.snaps',           bundle_version, release_from_branch, 'clean clean-integration test package publish-ivy publish-maven publish-package-build publish-package-download'),
     Repository.new(virgo_eclipse_repo_root, 'apps',                paths['apps'],                'org.eclipse.virgo.apps',            bundle_version, release_from_branch, default_targets),
@@ -103,7 +103,7 @@ elsif args[:product_release] == 'web-server'
 elsif args[:product_release] == 'virgo' 
   
   ALL_REPOS = [
-    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     nil, ripple_branch, 'clean'),
+    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',      paths['eclipse-mirror'],      'org.eclipse.virgo.eclipse-mirror',  bundle_version, release_from_branch, 'clean'),
     Repository.new(virgo_eclipse_repo_root, 'osgi-test-stubs',     paths['osgi-test-stubs'],     'org.eclipse.virgo.teststubs',       bundle_version, release_from_branch, default_targets),
     Repository.new(virgo_eclipse_repo_root, 'osgi-extensions',     paths['osgi-extensions'],     'org.eclipse.virgo.osgi',            bundle_version, release_from_branch, default_targets),
     Repository.new(virgo_eclipse_repo_root, 'util',                paths['util'],                'org.eclipse.virgo.util',            bundle_version, release_from_branch, default_targets),
@@ -124,7 +124,7 @@ elsif args[:product_release] == 'virgo'
 else
 
     ALL_REPOS = [
-    Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',     paths['eclipse-mirror'],     'org.eclipse.virgo.eclipse-mirror',     nil, ripple_branch, 'clean'),
+      Repository.new(virgo_eclipse_repo_root, 'eclipse-mirror',      paths['eclipse-mirror'],      'org.eclipse.virgo.eclipse-mirror',  bundle_version, release_from_branch, 'clean'),
       Repository.new(virgo_eclipse_repo_root, 'osgi-test-stubs',     paths['osgi-test-stubs'],     'org.eclipse.virgo.teststubs',       bundle_version, release_from_branch, default_targets),
       Repository.new(virgo_eclipse_repo_root, 'osgi-extensions',     paths['osgi-extensions'],     'org.eclipse.virgo.osgi',            bundle_version, release_from_branch, default_targets),
       Repository.new(virgo_eclipse_repo_root, 'util',                paths['util'],                'org.eclipse.virgo.util',            bundle_version, release_from_branch, default_targets),
